7352ec134ef56b98f0c7db2c542aa50070843fd0
troximi <135414788+troximi@users.noreply.github.com>
2023-10-16 13:56:57 +0000

    removed

--- LearnJava-main/jeffgeoff.java
+++
@@ -1,9 +0,0 @@
-public class jeffgeoff {
-    public static void main(String[] args) {
-        // System.out.println("Welcome to Java Programming");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
-
-    }
-}
