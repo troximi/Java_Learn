9538118b12adac93817ea9a0ffd250f748204631
troximi <135414788+troximi@users.noreply.github.com>
2023-11-13 11:52:57 +0000

    arrays in java

---
+++ Array.java
@@ -0,0 +1,20 @@
+public class Array {
+    public static void main(String[] args) throws Exception {
+
+        int[] numbers = {5,10,15,20,25 ,30,35,40,45,50,};
+
+        System.out.println("elemments in array");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("elemment at index_" + i + ": " + numbers[i]);
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        System.out.println("Sum of all elements in array is :" + sum );
+
+    }
+}
